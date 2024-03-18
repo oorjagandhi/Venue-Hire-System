79aaf178e9e5c0a3ae75594a1d70f56e9d79609c
Oorja Gandhi <oorjagandhi@gmail.com>
2024-03-19 12:04:51 +1300

    Adding a Java Venue class

---
+++ src/main/java/nz/ac/Venue.java
@@ -0,0 +1,18 @@
+package nz.ac;
+
+public class Venue {
+  private String venueName;
+  private String venueCode;
+  private int capacity;
+  private int hireFee;
+
+  
+  public Venue(String venueName, String venueCode, int capacity, int hireFee) {
+    this.venueName = venueName;
+    this.venueCode = venueCode;
+    this.capacity = capacity;
+    this.hireFee = hireFee;
+  }
+
+  
+}
