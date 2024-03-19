9c0c80bd2996ef22419d96d19730358ed33d82e5
Oorja Gandhi <oorjagandhi@gmail.com>
2024-03-19 21:20:39 +1300

    Added validation to check if venue name is empty and to check that venue codes are unique in createVenue method

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -15,6 +15,8 @@ public class VenueHireSystem {
     // TODO implement this method
     if (venues.isEmpty()){
       System.out.println("There are no venues in the system. Please create a venue first.");
+    } else {
+
     }
 
   }
@@ -22,6 +24,19 @@ public class VenueHireSystem {
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     // TODO implement this method
+
+    // Check if the venue name is empty
+    if (venueName.trim().isEmpty()){
+      System.out.println("Venue not created: venue name must not be empty.");
+    } 
+    
+    for (Venue venue : venues){
+      if (venue.getVenueCode().equals(venueCode)){
+        System.out.println("Venue not created: code '" + venueCode + "' is already used for '" + venue.getVenueName() + "'.");
+        return;
+      }
+
+    }
     
   }
 
