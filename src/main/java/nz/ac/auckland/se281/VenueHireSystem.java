0ebb6fe022d36e03809973780452048f7ee1b97b
Oorja Gandhi <oorjagandhi@gmail.com>
2024-03-19 22:26:24 +1300

    Added functionality to make new venue and add to the list if all validations passed

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -12,25 +12,19 @@ public class VenueHireSystem {
   public VenueHireSystem() {}
 
   public void printVenues() {
-    // TODO implement this method
     if (venues.isEmpty()){
       System.out.println("There are no venues in the system. Please create a venue first.");
-    } else {
-
     }
-
+    // TODO implement this method
   }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     // TODO implement this method
-
-    // Check if the venue name is empty
     if (venueName.trim().isEmpty()){
       System.out.println("Venue not created: venue name must not be empty.");
     } 
     
-    // Checking if the venue codes has already been used
     for (Venue venue : venues){
       if (venue.getVenueCode().equals(venueCode)){
         System.out.println("Venue not created: code '" + venueCode + "' is already used for '" + venue.getVenueName() + "'.");
@@ -38,7 +32,6 @@ public class VenueHireSystem {
       }
     }
 
-    // Check if the capacity is a number and is positive
     try {
       int tempCapacity = Integer.parseInt(capacityInput);
       if (tempCapacity <= 0){
@@ -49,7 +42,6 @@ public class VenueHireSystem {
       System.out.println("Venue not created: capacity must be a number.");
     }
 
-    // Check if the hire fee is a number and is positive
     try {
       int tempHireFee = Integer.parseInt(hireFeeInput);
       if (tempHireFee <= 0){
@@ -60,6 +52,11 @@ public class VenueHireSystem {
       System.out.println("Venue not created: hire fee must be a number.");
     }
     
+    Venue newVenue = new Venue(venueName, venueCode, capacityInput, hireFeeInput);
+        
+    venues.add(newVenue);
+    System.out.println("Successfully created venue '" + venueName + "' (" + venueCode + ").");
+
   }
 
   public void setSystemDate(String dateInput) {
