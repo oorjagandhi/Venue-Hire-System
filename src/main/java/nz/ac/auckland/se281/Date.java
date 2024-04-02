d7d53a29c3408676d592b26e6cfeeb048baf4292
Oorja Gandhi <oorjagandhi@gmail.com>
2024-04-02 22:18:09 +1300

    Added isBefore method to compare two dates

--- src/main/java/nz/ac/auckland/se281/Date.java
+++ src/main/java/nz/ac/auckland/se281/Date.java
@@ -5,10 +5,31 @@ public class Date {
   private int month;
   private int year;
 
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  public Date(String date) {
+    String[] dateParts = date.split("/");
+    this.day = Integer.parseInt(dateParts[0]);
+    this.month = Integer.parseInt(dateParts[1]);
+    this.year = Integer.parseInt(dateParts[2]);
+  }
+
+  public boolean isBefore(Date otherDate) {
+    // Checking if the year is after
+    if (this.year > otherDate.year) {
+      return false;
+    }
+
+    // Checking if the month is  after
+    if (this.month > otherDate.month) {
+      return false;
+    }
+
+    // Checking if the day is  after
+    if (this.day > otherDate.day) {
+      return false;
+    }
+
+    // Return true if the date is before
+    return true;
   }
 
   @Override
