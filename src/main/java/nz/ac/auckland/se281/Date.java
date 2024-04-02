d99f014972eb02521a51f76c50bf013126d19794
Oorja Gandhi <oorjagandhi@gmail.com>
2024-04-02 21:38:34 +1300

    Added a toString method into the Date class

--- src/main/java/nz/ac/auckland/se281/Date.java
+++ src/main/java/nz/ac/auckland/se281/Date.java
@@ -10,4 +10,21 @@ public class Date {
     this.month = month;
     this.year = year;
   }
+
+  @Override
+  public String toString() {
+
+    if (day < 10 && month < 10) {
+      return "0" + day + "/0" + month + "/" + year;
+    }
+    if (day < 10) {
+      return "0" + day + "/" + month + "/" + year;
+    }
+
+    if (month < 10) {
+      return day + "/0" + month + "/" + year;
+    }
+
+    return day + "/" + month + "/" + year;
+  }
 }
