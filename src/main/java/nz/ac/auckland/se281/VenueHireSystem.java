59f758931b98e760b156b349cdabdd36265bb47d
Oorja Gandhi <oorjagandhi@gmail.com>
2024-04-02 18:48:28 +1300

    Created the setSystemDate and printSystemDate method

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -1,25 +1,25 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
 
   ArrayList<Venue> venues = new ArrayList<Venue>();
+  private String systemDate = null;
 
   public VenueHireSystem() {}
 
   // Print the list of venues
   public void printVenues() {
-    if (venues.isEmpty()){
+    if (venues.isEmpty()) {
       // Print message if there are no venues
       MessageCli.NO_VENUES.printMessage();
-    } else if (venues.size() == 1){
+    } else if (venues.size() == 1) {
       // Print message if there is one venue
       MessageCli.NUMBER_VENUES.printMessage("is", "one", "");
-    } else if (venues.size() < 10){
+    } else if (venues.size() < 10) {
       // Print message if there are less than 10 venues
       MessageCli.NUMBER_VENUES.printMessage("are", digitToWords(venues.size()), "s");
     } else {
@@ -28,32 +28,35 @@ public class VenueHireSystem {
     }
 
     // Print the details of each venue
-    for (Venue venue : venues){
-      MessageCli.VENUE_ENTRY.printMessage(venue.getVenueName(), venue.getVenueCode(), venue.getCapacity(), venue.getHireFee());
+    for (Venue venue : venues) {
+      MessageCli.VENUE_ENTRY.printMessage(
+          venue.getVenueName(), venue.getVenueCode(), venue.getCapacity(), venue.getHireFee());
     }
   }
+
   // Convert a number to words
   private String digitToWords(int num) {
-    String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    if (num > 1 && num < 10){
+    String[] words = {
+      "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    };
+    if (num > 1 && num < 10) {
       return words[num];
     }
     return Integer.toString(num);
   }
 
-
-
   // Method to create a new venue
-  public void createVenue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+  public void createVenue(
+      String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     // Check if the venue name is empty
-    if (venueName == null || venueName.trim().isEmpty()){
+    if (venueName == null || venueName.trim().isEmpty()) {
       MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
       return;
-    } 
-    
+    }
+
     // Check if the venue code already exists
-    for (Venue venue : venues){
-      if (venue.getVenueCode().equals(venueCode)){
+    for (Venue venue : venues) {
+      if (venue.getVenueCode().equals(venueCode)) {
         MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, venue.getVenueName());
         return;
       }
@@ -62,12 +65,12 @@ public class VenueHireSystem {
     // Check if the capacity is positive
     try {
       int tempCapacity = Integer.parseInt(capacityInput);
-      if (tempCapacity <= 0){
+      if (tempCapacity <= 0) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
         return;
       }
       // Check if the capacity is a number
-    } catch (Exception e){
+    } catch (Exception e) {
       MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", "");
       return;
     }
@@ -75,16 +78,16 @@ public class VenueHireSystem {
     // Check if the hire fee is a positive number
     try {
       int tempHireFee = Integer.parseInt(hireFeeInput);
-      if (tempHireFee <= 0){
+      if (tempHireFee <= 0) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
         return;
       }
       // Check if the hire fee is a number
-    } catch (Exception e){
+    } catch (Exception e) {
       MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
       return;
     }
-    
+
     // If all validations passed, create the venue
     Venue newVenue = new Venue(venueName, venueCode, capacityInput, hireFeeInput);
     venues.add(newVenue);
@@ -92,15 +95,29 @@ public class VenueHireSystem {
   }
 
   public void setSystemDate(String dateInput) {
-    // TODO implement this method
+    this.systemDate = dateInput;
+    MessageCli.DATE_SET.printMessage(dateInput);
   }
 
   public void printSystemDate() {
-    // TODO implement this method
+    if (systemDate == null) {
+      System.out.println("Current system date is not set.");
+    } else {
+      MessageCli.CURRENT_DATE.printMessage(systemDate);
+    }
   }
 
   public void makeBooking(String[] options) {
     // TODO implement this method
+    // if (systemDate == null) {
+    //   MessageCli.BOOKING_NOT_MADE_DATE_NOT_SET.printMessage();
+    // }
+
+    // if (venues.isEmpty()) {
+    //   MessageCli.BOOKING_NOT_MADE_NO_VENUES.printMessage();
+    // }
+
+    // if (venue)
   }
 
   public void printBookings(String venueCode) {
