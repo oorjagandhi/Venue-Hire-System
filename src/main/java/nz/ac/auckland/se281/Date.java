2a8b217b1ac505a325c3539e96731dab66a9ba10
Oorja Gandhi <oorjagandhi@gmail.com>
2024-04-02 19:30:23 +1300

    Created and setup a Date class

---
+++ src/main/java/nz/ac/auckland/se281/Date.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se281;
+
+public class Date {
+  private int day;
+  private int month;
+  private int year;
+
+  public Date(int day, int month, int year) {
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+}
