31510a63a0b3d09b0980ea645d669f49656c7d3c
Oorja Gandhi <oorjagandhi@gmail.com>
2024-04-02 22:43:41 +1300

    Created a booking class and implement getter methods

---
+++ src/main/java/nz/ac/auckland/se281/Booking.java
@@ -0,0 +1,38 @@
+package nz.ac.auckland.se281;
+
+public class Booking {
+
+  private Venue venue;
+  private Date bookingDate;
+  private String customerEmail;
+  private int attendees;
+  private String bookingReference;
+
+  public Booking(Venue venue, Date bookingDate, String customerEmail, String attendees) {
+    this.venue = venue;
+    this.bookingDate = bookingDate;
+    this.customerEmail = customerEmail;
+    this.attendees = Integer.parseInt(attendees);
+    this.bookingReference = BookingReferenceGenerator.generateBookingReference();
+  }
+
+  public Venue getVenue() {
+    return venue;
+  }
+
+  public Date getBookingDate() {
+    return bookingDate;
+  }
+
+  public String getCustomerEmail() {
+    return customerEmail;
+  }
+
+  public int getAttendees() {
+    return attendees;
+  }
+
+  public String getBookingReference() {
+    return bookingReference;
+  }
+}
